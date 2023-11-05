d9b33a93de4335c2ff2f376e830d6fb03d6a450b
AlbatovK <albatovkonstantin@yandex.ru>
2023-11-05 13:14:15 +0300

    :art: ANTLR4: Lexers and parsers generated

--- buildSrc/src/main/kotlin/GradlePlugin.kt
+++ buildSrc/src/main/kotlin/GradlePlugin.kt
@@ -1,8 +1,14 @@
+
+import org.antlr.v4.runtime.tree.ParseTreeListener
+import org.antlr.v4.runtime.tree.ParseTreeWalker
+import org.codehaus.groovy.antlr.java.JavaLexer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class GradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        target.tasks.register("generateProjectStatistic") {
 
+        }
     }
 }
